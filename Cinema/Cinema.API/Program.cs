3f30b651ffb58c98f9aced717bccef6629c84158
IliaKim91 <paterfamelias@gmail.com>
2023-04-26 18:51:26 +0600

    Added AddScoped of unit of work

--- Cinema/Cinema.API/Program.cs
+++ Cinema/Cinema.API/Program.cs
@@ -1,4 +1,9 @@
 
+using Cinema.API.Data;
+using Cinema.API.Repositories.IRepositories;
+using Cinema.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cinema.API
 {
     public class Program
@@ -13,6 +18,11 @@ namespace Cinema.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(
+               builder.Configuration.GetConnectionString("DefualtConnection")));
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             var app = builder.Build();
 
